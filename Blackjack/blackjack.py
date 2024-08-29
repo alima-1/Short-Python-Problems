dc17d7a5f8c6d071840ec2c189bb4aa400d496f1
alima <alima.ayikoru@gmail.com>
2024-08-29 12:49:20 +0300

    Added the methods that take care of the player actions in the player class.

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -1,4 +1,5 @@
-import random, itertools              
+import random
+import itertools              
 
 
 def get_player_names():
@@ -7,38 +8,35 @@ def get_player_names():
         player_names = input("Please enter name(s) of player(s)."
                             "(Seperated by commas if there's more than one player)\n-->")
         player_names = [name.strip().capitalize() for name in player_names.split(",") if name.strip()]
-         
         #Handle No entries
         if len(player_names) == 0:
             print("No valid names entered.\nPlease try Again")
             continue
-
         #handle players exceeding max players
         if len(player_names) > 6:
             print(f"Only {max_players} players are allowed."
                 "Truncating the list to {max_players} players.")
             player_names = player_names[:max_players]
-
         #Handle dulpicates
         if len(player_names) != len(set(player_names)):
             print("Duplicate names found."
                   "\nPlease ensure all names are unique.")
             continue
-        
         break
-    
     return player_names
+
 
 class Player:
     def __init__(self,name) -> None:
         self.name = name
         self.bets = []
         self.hands = []
+        self.splits = 0
 
     def place_bet(self):
         max_bet = 500
         min_bet = 100
-
+        #validation loop
         while True:
             try:
                 bet = int(input(f"Place a bet (between {min_bet}-{max_bet}):"))
@@ -50,121 +48,152 @@ class Player:
             except ValueError:
                 print("that's not a valid integer. Please try again.")
     
-    def show_cards(self):
-        rows = ['','','','','']
-        
-        for hand in self.hands:
-            for (rank, suit) in hand:
-                rows[0] += f' ___ '
-                rows[1] += f'|{rank}  |'
-                rows[2] += f'| {suit} |'
-                rows[3] += f'|__{rank}|'
 
-            print()
-            print(player.name)
+    def show_cards(self):    
+        for hand in self.hands:
+            rows = ['','','','','']
+            for (rank, suit) in hand:
+                rows[0] += ' ___ '
+                rows[1] += f'|{rank:<2} |'
+                rows[2] += f'| {suit} |'
+                rows[3] += f'|_{rank:>2}|'
+            #print()
             for row in rows:
                 print(row)
+            print()  # Add space between hands
 
 
-    def getHandValue(self,player_hand):
+    def getHandValue(self,player_hand,hide_value=False):
+        if hide_value:
+            return "???"
         #evaluates the total of a hand
         total   = 0
         no_aces = 0 # keeps track of the number of aces in 
                     #a hand so it can added to the total last
-                    #so as to better decide w
-
-        for rank, _ in player_hand():
+                    #so as to better decide whether the ace can be a 1 or 11
+        for rank, _ in player_hand:
             if rank.isdigit():
                 total += int(rank)
             elif rank in ('J','Q','K'):
                 total += 10
             else:
                 no_aces += 1
-
+        #handle aces
         total += no_aces # adds aces as 1
         for _ in range(no_aces):
             if total + 10 <= 21: #(10 + 1 = 11) testin for ace as 11
                 total += 10
-
-
         return total
+    
+    def doubleDown(self, hand_index, card):
+        #the no of hands a player has will always match
+        #the no of bets placed.
+        doubled_bet = self.bets[hand_index] * 2
+        #replacing old bet with doubled bet
+        self.bets.insert(hand_index, doubled_bet)
+        self.hands[hand_index].append(card)
+
+    def hit(self,hand_index, card):
+        #adds a card to the respective hand
+        self.hands[hand_index].append(card)
+
+    def split(self, hand_index, dealer_object):
+        #Only split cards of the same rank not value
+        hand = self.hands[hand_index]
+        #check if the cards have the same rank
+        if len(hand) == 2 and hand[0][0] == hand[1][0]:
+            if self.splits >= 3:
+                print("Cannot split more than 3 times.")
+                return
+            first_card = hand.pop()
+            second_card = hand.pop()
+            #place an additional bet equal to the original
+            #bet
+            #there may be more bets than one so pick the 
+            #most recent bet
+            bet = self.bets[-1]
+            self.bets.append(bet)
+            #dealing cards
+            first_hand = [first_card, dealer_object.deal_cards()]
+            second_hand = [second_card,dealer_object.deal_cards()]
+            # Update self.hands with the new hands
+            self.hands[hand_index] = first_hand
+            self.append(second_hand)
+            self.splits += 1
+        else:
+            print("Cannot Split: The cards are not of the same rank.")
+
+    def stand(self):
+        pass
+
 
 
 class Dealer(Player):
     def __init__(self):
         self.name = "Dealer"
         self.hands= [] # B'se a player can have multiple hands (spliting)
+        self.deck = self._create_deck() 
     
     def _create_deck(self,num_decks = 3 ):
         rank = ('2', '3', '4','5', '6', '7','8','9', '10', 'J', 'Q', 'K', 'A')
         suit = {"Hearts":"\u2665", "Diamonds":"\u2666", "Clubs":"\u2663", "Spades":"\u2660"}
-
         #create deck of cards = to num of decks 
         deck = list(itertools.product(rank, suit.values())) * num_decks
-        
         #shuffle the created deck of cards
         random.shuffle(deck)
         return deck
     
     def deal_cards(self):
-        deck = self._create_deck()
-        #return tuple containing (rank,suit)
-        return deck.pop()
-        
+        return self.deck.pop()
     
-    def show_cards(self,hide=False):
-        rows = ['','','','','']
-
+    def show_cards(self,hide_first_card=False):
         for hand in self.hands:
+            rows = ['','','','','']
             for i, (rank, suit) in enumerate(hand):
-                if i == 0 and hide == True:
+                if i == 0 and hide_first_card == True:
                     rank = suit = '#'
-                    rows[0] += f' ___ '
-                    rows[1] += f'|{rank}  |'
+                    rows[0] += ' ___ '
+                    rows[1] += f'|{rank:<2} |'
                     rows[2] += f'| {suit} |'
-                    rows[3] += f'|__{rank}|'
+                    rows[3] += f'|_{rank:>2}|'
                 else:
                     rows[0] += f' ___ '
-                    rows[1] += f'|{rank}  |'
+                    rows[1] += f'|{rank:<2} |'
                     rows[2] += f'| {suit} |'
-                    rows[3] += f'|__{rank}|'
-
-            print()
-            print(player.name)
+                    rows[3] += f'|_{rank:>2}|'
+            #print()
             for row in rows:
                 print(row)
+            print()  # Add space between hands
 
-    
+
+    def getHandValue(self, player_hand, hide_value=False):
+        return super().getHandValue(player_hand, hide_value)
+
 
 if __name__ == "__main__":
     players = []
     #get the names/name of player
     player_names = get_player_names()
-     
     #create a player object for each player
-    #show the name of the player that has to place a bet
+    #show the name of the player that has to place a bet.
+    #get the players bet
     #add this player to a list to keep track and access it
-    for index,name in enumerate(player_names):
+    for name in player_names:
         player = Player(name)
         print(player.name)
         player.place_bet()
-        players.append(player)
-       
-       
-       
-    #insert dealer as first player in the list of players
+        players.append(player)   
+    #Initialise dealer
     dealer = Dealer()
-    players.insert(0,dealer)
-
-    #deal cards to all players
+    #deal initial cards to each player 
     for player in players:
-        hand = []
-        for _ in range(2):
-            hand.append(dealer.deal_cards())
-        player.hands.append(hand)
-            
+        player.hands.append([dealer.deal_cards(), dealer.deal_cards()])
+    #deal initial cards to dealer
+    dealer.hands.append([dealer.deal_cards(), dealer.deal_cards()])
+    #Display cards
+    print(f"{dealer.name}: {player.getHandValue(dealer.hands[0],hide_value=True)}")
+    dealer.show_cards(hide_first_card=True)        
     for player in players:
-        if player.name == 'Dealer':
-            player.show_cards(hide=True)
-        else:
-            player.show_cards()
+        print(f"{player.name}: {player.getHandValue(player.hands[0])}")
+        player.show_cards()
