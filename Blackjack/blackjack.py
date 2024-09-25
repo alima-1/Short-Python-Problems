0f19df76fe3959d871c8600071e1d21c9ee3f2b4
alima <alima.ayikoru1@gmail.com>
2024-09-25 08:59:07 +0300

    -Add new playHand method and refactor methods  doubledown,split. -add implementation details to stand method. -implement logic to check natural blackjack.

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -94,9 +94,9 @@ class Player:
         if len(self.hands) == 1 and len(first_hand) == 2:
             #the no of hands a player has will always match
             #the no of bets placed.
-            doubled_bet = self.bets[start_index] * 2
+            doubled_bet = self.bets.pop() * 2
             #replacing old bet with doubled bet
-            self.bets.insert(start_index, doubled_bet)
+            self.bets.append(doubled_bet)
             self.hands[start_index].append(dealer_object.deal_cards())
         else:
             print("You can only double down on the initial 2 card hand.")
@@ -113,8 +113,8 @@ class Player:
             if self.splits >= 3:
                 print("Cannot split more than 3 times.")
                 return
-            first_card = hand.pop()
-            second_card = hand.pop()
+            #split cards
+            split_card = hand.pop()
             #place an additional bet equal to the original
             #bet
             #there may be more bets than one so pick the 
@@ -122,17 +122,40 @@ class Player:
             bet = self.bets[-1]
             self.bets.append(bet)
             #dealing cards
-            first_hand = [first_card, dealer_object.deal_cards()]
-            second_hand = [second_card,dealer_object.deal_cards()]
-            # Update self.hands with the new hands
-            self.hands[hand_index] = first_hand
-            self.hands.append(second_hand)
+            self.hands[hand_index].append(dealer_object.deal_cards())
+            new_hand = [split_card,dealer_object.deal_cards()]
+            # Update self.hands with the new hand
+            self.hands.append(new_hand)
             self.splits += 1
         else:
             print("Cannot Split: The cards are not of the same rank.")
 
     def stand(self, hand_index):
         print(f"{self.name} stands on Hand #{hand_index + 1}.")
+ 
+def playHand(self,hand_index,dealer_object):
+    moves = ['D -> doubledown', 'SP -> split','S -> stand','H ->hit']
+    while True:
+        hand_value = self.getHandValue(self.hands[hand_index])
+        print(f"{self.name} Hand#{hand_index + 1}: {hand_value}")
+        if hand_value <= 21:
+            action = input('\n'.join(moves) +'>>>').upper()
+            if action =='D':
+                self.doubleDown(dealer_object)
+                break
+            elif action == 'SP':
+                self.split(hand_index,dealer_object)
+                break
+            elif action == 'S':
+                self.stand(hand_index)
+                break
+            elif action == 'H':
+                self.hit(hand_index,dealer_object)
+            else:
+                print("Invalid Action. Please choose again ")
+        else:
+            print(f"{self.name} Hand #{hand_index + 1} Busted! You lose.")
+            break
 
 
 
@@ -186,7 +209,6 @@ if __name__ == "__main__":
     #get the names/name of player
     player_names = get_player_names()
     #create a player object for each player
-    #show the name of the player that has to place a bet.
     #get the players bet
     #add this player to a list to keep track and access it
     for name in player_names:
@@ -196,14 +218,40 @@ if __name__ == "__main__":
         players.append(player)   
     #Initialise dealer
     dealer = Dealer()
-    #deal initial cards to each player 
+    #deal 2 initial cards to each player 
     for player in players:
         player.hands.append([dealer.deal_cards(), dealer.deal_cards()])
-    #deal initial cards to dealer
+    #deal 2 initial cards to dealer
     dealer.hands.append([dealer.deal_cards(), dealer.deal_cards()])
-    #Display cards
+    #Display player's cards
     print(f"{dealer.name}: {player.getHandValue(dealer.hands[0],hide_value=True)}")
     dealer.show_cards(hide_first_card=True)        
     for player in players:
         print(f"{player.name}: {player.getHandValue(player.hands[0])}")
         player.show_cards()
+    #check for a nautral blackjack
+    dealer_init_hand_val = dealer.getHandValue(dealer.hands[0])
+    for index,player in enumerate(players):
+        player_init_hand_val = player.getHandValue(player.hands[0])
+        if player_init_hand_val == 21:
+            if dealer_init_hand_val == 21:
+               print(f"A Tie! Both {player.name} and the dealer have Blackjack.") 
+               del player[index] 
+            else:
+                print(f"{player.name} wins with a Blackjack!")
+                del player[index] 
+        elif dealer_init_hand_val == 21:
+            print(f"{player.name} loses. Dealer has Blackjack.")
+            del player[index] 
+        else:
+            print(f"Neither {player.name} nor dealer have Blackjack. Game continues.")
+         
+    #player actions
+    if players:
+        for player in players:
+        #loops through each hand in hands
+            for index in range(len(player.hands)):
+                print(f"{player.name} #Hand{index+1}:")
+                player.playHand(index, dealer)
+
+
