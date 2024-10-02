35d010a59c6c1506f2f4e7a92d1f922a2459602d
alima <alima.ayikoru1@gmail.com>
2024-10-02 14:57:54 +0300

    Implement get card value method for card class.

--- blacjack/card.py
+++ blacjack/card.py
@@ -4,15 +4,26 @@ class Card:
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
+    
+    def get_card_value(self):
+        if self.rank.isdigit():
+            return int(self.rank)
+        
+        elif self.rank in ['J','Q','K']:
+                return 10
+        
+        elif self.rank == 'A':
+            return 11 #or 1, depending on the game logic
+        
 
 class Deck:
-    def __init__(self, num_decks):
-        self.num_decks = num_decks
-        self.cards = self.createDeck(self.num_decks)
+    def __init__(self, num_of_decks):
+        self.num_of_decks = num_of_decks
+        self.cards = self.createDeck(self.num_of_decks)
         #shuffle the deck
         self._shuffle()
 
-    def createDeck(self, num_decks):
+    def createDeck(self, num_of_decks):
         ranks = ['2','3','4','5','6','7','8','9','10',
                  'J','Q','K','A']
         suits = {'Hearts':'\u2665', 'Diamond': '\u2666',
@@ -21,7 +32,7 @@ class Deck:
         cards  = [Card(rank, suit) 
                  for rank in ranks
                  for suit in suits.values()
-                 for _ in range(self.num_decks)]
+                 for _ in range(self.num_of_decks)]
         return cards
     
     def _shuffle(self):
@@ -30,6 +41,3 @@ class Deck:
     def dealCard(self):
         return self.cards.pop()
 
-if __name__ == '__main__':
-    deck = Deck(3)
-    print(len(deck.cards))
